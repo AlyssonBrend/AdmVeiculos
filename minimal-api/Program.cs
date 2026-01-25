79b5fcdb0e522824d541d552b825c7f2bf223d99
Alysson Brendo Araujo Canuto <alyssonestud@hotmail.com>
2026-01-25 16:48:05 +0000

    2
    2

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -3,4 +3,17 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if(loginDTO.Email =="adm@teste.com" && loginDTO.Senha == "Adm@1234")
+    {
+        return Results.Ok("Login com sucesso");
+    }
+    else Results.Unauthorized();
+});
+public class LoginDTO
+{
+    public string Email {get; set} = default;
+    public string Senha {get; set} = default;
+
+}
 app.Run();

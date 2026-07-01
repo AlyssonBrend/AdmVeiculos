fb87c42b1abe95e3c8b689269d4514ec4394600b
Alysson Brendo Araujo Canuto <alyssonestud@hotmail.com>
2026-07-01 22:27:38 +0100

    2
    2

---
+++ minimal-api/Dominio/Entidades/Admininstador.cs
@@ -0,0 +1,14 @@
+namespace MinimalApi.Dominio.Entidades;
+public class Administrador
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id {get; set; }
+    [Required]
+    [StringLength(250)]
+    public string Email {get; set; } = default!;
+    [StringLength(50)]    
+    public string Senha {get; set; } = default!;
+    [StringLength(10)]
+    public string Perfil {get; set; } = default!;
+}

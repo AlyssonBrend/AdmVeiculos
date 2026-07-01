b6e68bccdadaa78f869a53aabe3383eede7d8c3b
Alysson Brendo Araujo Canuto <alyssonestud@hotmail.com>
2026-07-01 21:50:54 +0100

    Tentando corrigir bug

--- minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45cf4f099cf08c3e3dcc238236f8d861977a645e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad47eb7e31c13118e89946871ba0554079585487")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
